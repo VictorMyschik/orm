90a2eaead5c03d4244973b086ad99b61123ffc1a
Victor Myschik <vmyschik@quintegro.com>
2023-08-07 21:17:13 +0200

    Fix tests

--- ORM.php
+++ ORM.php
@@ -3,6 +3,7 @@
 namespace App\Models\ORM;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
 
 class ORM extends Model
@@ -14,21 +15,9 @@ class ORM extends Model
     return with(new static)->getTable();
   }
 
-  /**
-   * Load object (get last result)
-   *
-   * @return static|object|null
-   */
-  public static function loadBy(?int $value)
+  public static function loadBy(?int $value): ?static
   {
-    if(!$value) {
-      return null;
-    }
-
-    $className = static::class;
-    $object = $className::find($value);
-
-    return $object;
+    return static::find((int)$value);
   }
 
   public function id(): ?int
@@ -36,14 +25,12 @@ class ORM extends Model
     return $this->attributes['id'] ?? null;
   }
 
-  public static function loadByOrDie(?string $value)
+  /**
+   * @throws ModelNotFoundException
+   */
+  public static function loadByOrDie(int $value): static
   {
-    $object = self::loadBy($value);
-
-    $msg = 'Object ' . self::getTableName() . ' not loaded: id' . $value;
-    abort_if(!$object, Response::HTTP_INTERNAL_SERVER_ERROR, $msg);
-
-    return $object;
+    return self::findOrFail($value);
   }
 
   // Disable Laravel time fields
@@ -52,37 +39,41 @@ class ORM extends Model
 
   public function save_mr(bool $skipFlushAffectedCaches = false): ?int
   {
-    if(method_exists($this, 'beforeSave')) {
+    if (method_exists($this, 'beforeSave')) {
       $this->beforeSave();
     }
 
     $this->save();
 
-    if(method_exists($this, 'afterSave')) {
+    if (method_exists($this, 'afterSave')) {
       $this->afterSave();
     }
 
-    if($skipFlushAffectedCaches && method_exists($this, 'flushAffectedCaches')) {
+    if ($skipFlushAffectedCaches && method_exists($this, 'flushAffectedCaches')) {
       $this->flushAffectedCaches();
     }
 
-    if(method_exists($this, 'flush')) {
+    if (method_exists($this, 'flush')) {
       $this->flush();
     }
 
     return $this->id();
   }
 
-  public function delete_mr(bool $skipAffectedCache = true): bool
+  public function delete_mr(bool $skipFlushAffectedCaches = false): bool
   {
-    if(method_exists($this, 'beforeDelete')) {
+    if (method_exists($this, 'beforeDelete')) {
       $this->beforeDelete();
+    }
+
+    if (method_exists($this, 'flush')) {
+      $this->flush();
     }
 
     $results = $this->delete();
     abort_if(!$results, Response::HTTP_INTERNAL_SERVER_ERROR, 'Object was not deleted');
 
-    if($skipAffectedCache && method_exists($this, 'afterDelete')) {
+    if ($skipFlushAffectedCaches && method_exists($this, 'afterDelete')) {
       $this->afterDelete();
     }
 
